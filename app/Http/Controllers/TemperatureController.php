8d17d0ce78b68147b02258a5f5cea3ce8703c725
Gabriel <gabrielfuja001@gmail.com>
2019-12-05 14:10:48 +0000

    validation

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Temperature;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class TemperatureController extends Controller
 {
@@ -25,10 +26,16 @@ class TemperatureController extends Controller
      */
     public function search(Request $request)
     {
-        $request->validate([
-            'from' => 'required',
-            'to' => 'required',
+
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|date',
+            'to' => 'required|date',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->messages()->first()]);
+        }
+
         $from = $request->input("from");
         $to = $request->input("to");
         $result = Temperature::whereBetween('dateRecorded', array($from, $to))->get();
@@ -51,13 +58,25 @@ class TemperatureController extends Controller
      */
     public function update(Request $request)
     {
+
         $jsonBody = json_decode($request->getContent(), true);
+        $rules = [
+            'date' => 'required|date',
+            'temperature' => 'required|integer',
+        ];
+
+        $validator = Validator::make($jsonBody, $rules);
+
+        if ($validator->fails()) {
+            return response()->json(['response' => $validator->messages()->first()]);
+        }
+
         $date = $jsonBody["date"];
         $temperature = $jsonBody["temperature"];
-        //dd($jsonBody);
+
         $update = Temperature::where('dateRecorded', $date)
             ->update(['temperature' => $temperature]);
-            
+
         if ($update)
             $message = response()->json([
                 'response' => 'OK! record updated'
